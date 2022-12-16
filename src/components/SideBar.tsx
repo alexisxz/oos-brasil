c34f4645db4abb490281c437d13532e618e7bde9
Alexis Matos <alexisxzinho@hotmail.com>
2022-12-16 20:27:51 +0100

    fix: link to the home page on logo

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -3,13 +3,19 @@ import Link from 'next/link'
 import React from 'react'
 import Logo from '../../public/logos-oosbrasil_lightgray.png'
 import styles from '../styles/Home.module.css'
+import { useRouter } from 'next/router'
 
 type Props = {}
 
 export default function SideBar({ }: Props) {
+    const router = useRouter()
+
+    const handleOnClick = () => {
+        router.replace('/')
+    }
     return (
         <div className={styles.sidebar}>
-            <Image src={Logo} alt="OOS Logo" className={styles.headerLogo} />
+            <Image src={Logo} alt="OOS Logo" className={styles.headerLogo} onClick={handleOnClick} />
             <nav className={styles.nav}>
                 <Link href='/'>Organizações</Link>
                 <Link href='/livros'>Livros</Link>
