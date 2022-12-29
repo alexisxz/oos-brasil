b41b745a91d0d3f3c1d9078183ad815fdb7bffa1
Alexis Matos <alexisxzinho@hotmail.com>
2022-12-29 13:45:16 +0100

    fix: cleaning project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import SideBar from '../components/SideBar'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
